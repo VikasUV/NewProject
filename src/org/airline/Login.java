f493adc23f9895b8d39983a8a64b379f66f42c0c
VikasUV <vikas.uriti@gmail.com>
2021-10-08 20:57:40 +0530

    Username Functionality

--- src/org/airline/Login.java
+++ src/org/airline/Login.java
@@ -1,5 +1,11 @@
 package org.airline;
 
 public class Login {
-
+	
+	public static void userName() {
+  System.out.println("username");
+  }
+public static void main(String[] args) {
+	userName();
+}
 }
