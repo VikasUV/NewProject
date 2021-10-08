fefe5360caa5aa5d9c27c973a9824cafcbaa632a
VikasUV <vikas.uriti@gmail.com>
2021-10-08 21:07:05 +0530

    Password Functionality

--- src/org/airline/Login.java
+++ src/org/airline/Login.java
@@ -2,10 +2,10 @@ package org.airline;
 
 public class Login {
 	
-	public static void userName() {
-  System.out.println("username");
+	public static void password() {
+  System.out.println("password");
   }
 public static void main(String[] args) {
-	userName();
+	password();
 }
 }
